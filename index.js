fd761e1c6fedaa5fa353598e057dd3f5f9ae5846
chase <chasebaker21@gmail.com>
2019-11-06 18:08:53 -0500

    fixed while loop

--- index.js
+++ index.js
@@ -40,15 +40,17 @@ for(let i=0; i < lifeEvents.length; i++) {
 let counter = 0;
 
 while(true) {
-  let randomNumber = Math.floor(Math.random() * 11);
+  let randomNumber = Math.floor(Math.random() * 10) + 1;
 
     if (randomNumber !== 5) {
-        counter++;
         console.log(randomNumber + " !== 5");
-        break;
+        counter++;
+        
+
     } else {
+      console.log("5 === 5. it took " + counter + " iterations to randomly generate the number 5.")
       counter++;
-        console.log("5 === 5. it took " + counter + " iterations to randomly generate the number 5.")
-    } break;
+      break;
+    }
 }
 
